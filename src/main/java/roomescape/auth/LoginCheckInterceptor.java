103dda5022ae411d85596d2a489da5d70c3c5d74
kangrae <kangrae0501@gmail.com>
2026-05-19 20:02:41 +0900

    fix: 인터셉터에서 오류를 던져서 exception handler를 타도록 변경

--- src/main/java/roomescape/auth/LoginCheckInterceptor.java
+++ src/main/java/roomescape/auth/LoginCheckInterceptor.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
+import roomescape.common.exception.UnauthorizedException;
 
 @Component
 public class LoginCheckInterceptor implements HandlerInterceptor {
@@ -19,8 +20,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
     ) throws Exception {
         HttpSession session = request.getSession(false);
         if (session == null || session.getAttribute(LOGIN_MEMBER_ID) == null) {
-            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-            return false;
+            throw new UnauthorizedException();
         }
         return true;
 
